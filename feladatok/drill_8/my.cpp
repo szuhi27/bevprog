025e15c9e33f5086a0b29b104c6b335d889613c8
szuhi27 <43464694+szuhi27@users.noreply.github.com>
2018-12-07 23:00:30 +0100

    Add files via upload

---
+++ feladatok/drill_8/my.cpp
@@ -0,0 +1,33 @@
+#include "my.h"
+#include "std_lib_facilities.h"
+
+int foo;
+
+void print_foo(){
+ cout<< foo;
+}
+
+void print(int i){
+ cout<< i;
+}
+
+void swap_v(int a, int b){
+ int temp; 
+ temp = a; 
+ a=b; 
+ b=temp;
+}
+
+void swap_r(int& a, int& b){
+ int temp;
+ temp = a;
+ a=b;
+ b=temp;
+}
+
+/*swap_cr(const int& a, const int& b){
+ int temp;
+ temp = a;
+ a=b;
+ b=temp;
+}*/
---
+++ feladatok/drill_8/use.cpp
@@ -0,0 +1,22 @@
+#include "my.h"
+
+int main(){
+
+ foo=7;
+ print_foo();
+ print(99);
+
+ int x = 7;
+ int y =9;
+  swap_r(x,y);
+  swap_v(7,9);
+ /*const int cx = 7;
+ const int cy = 9;
+  swap_cr(cx,cy);
+  swap_cr(7.7,9.9);
+ double dx = 7.7;
+ double dy = 9.9;
+  swap_?(dx,dy);
+  swap_?(7.7,9.9);*/
+ return 0;
+}
